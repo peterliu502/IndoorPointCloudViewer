91902ed41a638036a0dc5ec5340f5c9c8a3914d5
e <798551899@qq.com>
2021-05-09 19:33:54 +0200

    add ply2txt.py and txt files

---
+++ ply2txt.py
@@ -0,0 +1,11 @@
+import os
+
+filePath = '.\\data\\'
+ls_path = os.listdir(filePath)
+for filename in ls_path:
+    if filename[-5:] == "A.ply":
+        with open(filePath + filename, 'r+', encoding='utf-8') as file_read:
+            with open(filePath + filename[:-3] + "txt", 'w', encoding='utf-8') as file_write:
+                for line in file_read.readlines():
+                    if line[0].isdigit() or line[0] == "-":
+                        file_write.write(line)
